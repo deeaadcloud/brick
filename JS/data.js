02cdef6ece9ce1ddbd49cb5cc4ca9f1bdfe669d6
DeadCloud <pavel2012ig@gmail.com>
2023-04-22 00:05:12 +0300

    new js1

--- JS/data.js
+++ JS/data.js
@@ -1,6 +1,6 @@
 
 (async () => {
-    let DB = await(await fetch('../data.json')).json();
+    let DB = await(await fetch('./data.json')).json();
  
    var root = document.getElementById('root');
    DB.forEach(element => root.insertAdjacentHTML('beforebegin', ` <td>${element.name}</td><td>${element.long}</td><td>${element.width}</td><td>${element.height}</td><td><a class="open-popup" href="https://wa.me/79879970471">${element.price}</a></td></tr>`));
