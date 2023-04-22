151d2938ee421a34e24515dffa07129777fc2dbf
Pavel Ignatev <74013392+deeaadcloud@users.noreply.github.com>
2023-04-22 16:11:37 +0300

    Update data.js

--- JS/data.js
+++ JS/data.js
@@ -1,7 +1,7 @@
 
 
 (async () => {
-    let DB = await(await fetch('../JS/data.json')).json();
+    let DB = await(await fetch('../brick/JS/data.json')).json();
  
    var root = document.getElementById('root');
    DB.forEach(element => root.insertAdjacentHTML('beforebegin', ` <td>${element.name}</td><td>${element.long}</td><td>${element.width}</td><td>${element.height}</td><td><a class="open-popup" href="https://wa.me/79879970471">${element.price}</a></td></tr>`));
